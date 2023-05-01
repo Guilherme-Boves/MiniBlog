55cb2ad4ee9bbed9eb70870968df64f582088c6c
Guilherme Boves <ghboves@gmail.com>
2023-05-01 17:28:21 -0300

    Sistema de autenticação

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from 'react'
 import styles from "./Register.module.css"
+import { useAuthentication } from '../../hooks/useAuthentication';
 
 const Register = () => {
 
@@ -10,7 +11,9 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("")
 
-  const handleSubmit = (e) => {
+  const {createUser, error: authError, loading} = useAuthentication();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(displayName, email, password, confirmPassword)
 
@@ -27,8 +30,16 @@ const Register = () => {
       return
     }
 
-    console.log(user)
+    const res = await createUser(user);
+
+    console.log(res)
   }
+
+  useEffect(() => {
+    if(authError) {
+      setError(authError);
+    }
+  }, [authError])
 
   return (
     <div className={styles.register}>
@@ -79,7 +90,12 @@ const Register = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && ( 
+          <button disabled className="btn">
+            Aguarde...
+          </button> 
+        )}
         {error && <p className='error'>{error}</p>}
       </form>
     </div>
